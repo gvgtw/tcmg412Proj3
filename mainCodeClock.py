79f494f0a82ef9bff40b36d46119473363917804
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-18 20:36:24 +0000

    Update mainCodeClock.py
    Changed the libraries that are being used, adding print lines to track outputs. Set several variables

--- mainCodeClock.py
+++ mainCodeClock.py
@@ -1,12 +1,24 @@
-from urllib.request import urlretrieve
+from datetime import datetime
+import urllib.request
+import os
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 
+local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+
+totalRequest = 0
+reqTimeFrame = 0
 def reader(filename):
-  with open(filename) as f:
-    log = f.read()
-    print(log)
-    
-if __name__ == '__main__':
-  reader('log')
+    with open(filename) as f:
+        log = f.read()
+        for log in f:
+            #timestamp = datetime.datetime.strptime(line.strip(), '[%d/%b/%Y:%H:%M:%S %z]')
+            print(log)
+   
+
+
+
+print("Total request made within the log: ", totalRequest)
+print("Total request made within the last 365 days: ", reqTimeFrame)
