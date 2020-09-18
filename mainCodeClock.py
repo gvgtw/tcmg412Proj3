f35d8d4d695c30259dafcf0adf1f9686b6b31939
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-18 21:25:36 +0000

    Update mainCodeClock.py
    Added an input variable to ask the use how many days back will be counted. Still encountering an error with the requested time frame not being counted properly.

--- mainCodeClock.py
+++ mainCodeClock.py
@@ -7,7 +7,7 @@ LOCAL_FILE = 'local_copy.log'
 
 local_file, headers = urllib.request.urlretrieve(URL_PATH, LOCAL_FILE)
 
-
+timeFrame = int(input('how many days back in the log file would you like counted: '))
 
 totalRequest = 0
 reqTimeFrame = 0
@@ -17,10 +17,14 @@ with open(LOCAL_FILE) as f:
         if len(line) < 10:
             continue
         timestamp = datetime.strptime(line[3].replace("[", ""), "%d/%b/%Y:%H:%M:%S")
+        totalRequest += 1
+        diffTime = datetime(1995,10,11,14,14,17) - timestamp
+        if diffTime.days <= timeFrame:
+            reqTimeFrame += 1
     print("This is a Test line: Spot #1")
    
 
 
 
 print("Total request made within the log: ", totalRequest)
-print("Total request made within the last 365 days: ", reqTimeFrame)
+print("Total request made within the last ", timeFrame, " days: ", reqTimeFrame)
