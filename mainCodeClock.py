4b7941443a3305ac75e85dd46391722cbbdb2e25
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-18 21:00:31 +0000

    Update mainCodeClock.py
    Attempted add changing how my data is read by the program. Encountering many errors.

--- mainCodeClock.py
+++ mainCodeClock.py
@@ -5,17 +5,18 @@ import os
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+local_file, headers = urllib.request.urlretrieve(URL_PATH, LOCAL_FILE)
+
 
 
 totalRequest = 0
 reqTimeFrame = 0
-def reader(filename):
-    with open(filename) as f:
-        log = f.read()
-        for log in f:
-            #timestamp = datetime.datetime.strptime(line.strip(), '[%d/%b/%Y:%H:%M:%S %z]')
-            print(log)
+with open(LOCAL_FILE) as f:
+    for line in f:
+        if len(line) < 10:
+            continue
+        timestamp = datetime.strptime(line.strip("["), "%d/%b/%Y:%H:%M:%S")
+    print("This is a Test line: Spot #1")
    
 
 
