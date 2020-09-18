c0f722b174955e449e4cf07a00d49e8f31251742
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-18 18:56:00 +0000

    Update mainCodeClock.py

--- mainCodeClock.py
+++ mainCodeClock.py
@@ -2,3 +2,11 @@ from urllib.request import urlretrieve
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
+
+def reader(filename):
+  with open(filename) as f:
+    log = f.read()
+    print(log)
+    
+if __name__ == '__main__':
+  reader('log')
