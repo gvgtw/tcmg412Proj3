1c37c7445841d2a8d107b3e41f2c59230c2ba275
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-25 20:25:03 +0000

    Update mainCodeBlock.py
    Significant addition of code. Performs counting functions and averages the accesses through the days, weeks, and months. The math portion is performed outside the log stream.

--- mainCodeBlock.py
+++ mainCodeBlock.py
@@ -10,34 +10,65 @@ if (os.path.isfile(LOCAL_FILE) == False):
     local_file, headers = urllib.request.urlretrieve(URL_PATH, LOCAL_FILE)
 
 pLog = [[]*5]*1
-avgAccess = []
-avgAccessVariable = 0
+avgAccess = [0]
+avgAccessMonthly = [0]
+uniqueDays = 0
+uniqueMonths = 0
 
 totalRequest = 0
 totalFailedRequest = 0
 totalRedirectedRequest = 0
+previousDate = 0
+previousMonth = 0
+
 with open(LOCAL_FILE) as f:
     for line in f:
         line = line.split(" ")
         if len(line) != 10:
             continue
         temp = int(line[8]) 
+
+        # Counting portion for failed and redirected functions
         if (temp >= 400) and (temp <= 499):
             totalFailedRequest += 1
         elif (temp >= 300) and (temp <= 399):
             totalRedirectedRequest += 1
+
+        # Counting portion for time functions
         timestamp = datetime.strptime(line[3].replace("[", ""), "%d/%b/%Y:%H:%M:%S")
-        temp2 = timestamp.date
-        if temp2 == temp3:
-            avgAccess[avgAccessVariable] += 1
-        else
-            temp3 = temp2
-            avgAccessVariable +=1    
+        temp2 = timestamp.day
+        if temp2 == previousDate:
+            avgAccess[uniqueDays] += 1
+        else:
+            previousDate = timestamp.day
+            uniqueDays += 1
+            avgAccess.append(1) 
+
+        temp3 = timestamp.month
+        if temp3 == previousMonth:
+            avgAccessMonthly[uniqueMonths] += 1
+        else:
+            previousMonth = timestamp.month
+            uniqueMonths += 1
+            avgAccessMonthly.append(1)
+        
+
         totalRequest += 1
         pLog.append(line)
+        """
         if totalRequest == 100:
             break
-    #print("This is a Test line: Spot #1")
+        """
+    # Auto Closing of file stream
+
+sumofDays = sum(avgAccess)
+totalofDays = len(avgAccess) - 1
+averageofDays = sumofDays / totalofDays
+averageofWeeks = sumofDays / (totalofDays / 7)
+
+sumofMonths = sum(avgAccessMonthly)
+totalofMonths = len(avgAccessMonthly) - 1
+averageofMonths = sumofMonths / totalofMonths
 
 del pLog[0]
 
@@ -45,3 +76,7 @@ del pLog[0]
 print("Total request made within the log: ", totalRequest)
 print("What percentage of the request were not successful: ", totalFailedRequest/totalRequest * 100)
 print("What percentage of the request were redirected elsewhere: ", totalRedirectedRequest/totalRequest * 100)
+print("Unique Days: ", uniqueDays)
+print("Average access request per day: ", averageofDays)
+print("Average access request per week: ", averageofWeeks)
+print("Average access request per month: ", averageofMonths)
