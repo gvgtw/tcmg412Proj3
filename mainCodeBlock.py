3b38e78ff171e00160150c20243b369ca5baa0d3
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-25 19:43:36 +0000

    Update mainCodeBlock.py
    Added portion to determine total fail and redirect request

--- mainCodeBlock.py
+++ mainCodeBlock.py
@@ -10,17 +10,29 @@ if (os.path.isfile(LOCAL_FILE) == False):
     local_file, headers = urllib.request.urlretrieve(URL_PATH, LOCAL_FILE)
 
 pLog = [[]*5]*1
-
+avgAccess = []
+avgAccessVariable = 0
 
 totalRequest = 0
-totalLogs = 0
-reqTimeFrame = 0
+totalFailedRequest = 0
+totalRedirectedRequest = 0
 with open(LOCAL_FILE) as f:
     for line in f:
         line = line.split(" ")
-        if len(line) < 10:
+        if len(line) != 10:
             continue
-        datetime.strptime(line[3].replace("[", ""), "%d/%b/%Y:%H:%M:%S")
+        temp = int(line[8]) 
+        if (temp >= 400) and (temp <= 499):
+            totalFailedRequest += 1
+        elif (temp >= 300) and (temp <= 399):
+            totalRedirectedRequest += 1
+        timestamp = datetime.strptime(line[3].replace("[", ""), "%d/%b/%Y:%H:%M:%S")
+        temp2 = timestamp.date
+        if temp2 == temp3:
+            avgAccess[avgAccessVariable] += 1
+        else
+            temp3 = temp2
+            avgAccessVariable +=1    
         totalRequest += 1
         pLog.append(line)
         if totalRequest == 100:
@@ -29,5 +41,7 @@ with open(LOCAL_FILE) as f:
 
 del pLog[0]
 
-print (pLog)
+# print (pLog)
 print("Total request made within the log: ", totalRequest)
+print("What percentage of the request were not successful: ", totalFailedRequest/totalRequest * 100)
+print("What percentage of the request were redirected elsewhere: ", totalRedirectedRequest/totalRequest * 100)
