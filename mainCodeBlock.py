efc22fc9563311534863f731e7f5d42d6ba0bb5d
Todd J Weaver <gvgtw@users.noreply.github.com>
2020-09-25 19:04:56 +0000

    Update mainCodeBlock.py
    Added an array function to allow me to more effectively parse through the data

--- mainCodeBlock.py
+++ mainCodeBlock.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import urllib.request
 import os
+from array import *
 
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
@@ -8,24 +9,25 @@ LOCAL_FILE = 'local_copy.log'
 if (os.path.isfile(LOCAL_FILE) == False):
     local_file, headers = urllib.request.urlretrieve(URL_PATH, LOCAL_FILE)
 
-timeFrame = int(input('how many days back in the log file would you like counted: '))
+pLog = [[]*5]*1
+
 
 totalRequest = 0
+totalLogs = 0
 reqTimeFrame = 0
 with open(LOCAL_FILE) as f:
     for line in f:
         line = line.split(" ")
         if len(line) < 10:
             continue
-        timestamp = datetime.strptime(line[3].replace("[", ""), "%d/%b/%Y:%H:%M:%S")
+        datetime.strptime(line[3].replace("[", ""), "%d/%b/%Y:%H:%M:%S")
         totalRequest += 1
-        diffTime = datetime(1995,10,11,14,14,17) - timestamp
-        if diffTime.days <= timeFrame:
-            reqTimeFrame += 1
-    # print("This is a Test line: Spot #1")
-   
+        pLog.append(line)
+        if totalRequest == 100:
+            break
+    #print("This is a Test line: Spot #1")
 
+del pLog[0]
 
-
+print (pLog)
 print("Total request made within the log: ", totalRequest)
-print("Total request made within the last ", timeFrame, " days: ", reqTimeFrame)
